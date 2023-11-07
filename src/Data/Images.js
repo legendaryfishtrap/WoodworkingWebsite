504edbbe6604f69fac47eaa522d01cc24f8db1a4
legendaryfishtrap <legendaryfishtrap@proton.me>
2023-11-06 16:46:28 -0800

    Testing commit hooks

---
+++ src/Data/Images.js
@@ -0,0 +1,31 @@
+export const Images = [
+  {
+    image: "1.jpeg",
+    title: "Cup",
+    project: "cup",
+  }
+,
+  {
+    image: "1.jpeg",
+    title: "Cup1",
+    project: "cup1",
+  }
+,
+  {
+    image: "1.webp",
+    title: "Cup2",
+    project: "cup2",
+  }
+,
+  {
+    image: "1.webp",
+    title: "Cup3",
+    project: "cup3",
+  }
+,
+  {
+    image: "1.avif",
+    title: "Cup5",
+    project: "cup5",
+  }
+]
---
+++ src/Data/Projects.js
@@ -0,0 +1,32 @@
+export const Projects = {
+  cup: {
+    projectName: "cup",
+    projectDescription: "Wow a cup",
+    projectImages: ["1.jpeg","2.jpeg","3.jpeg"],
+    projectThumbnails: ["thumbnails/1.jpeg","thumbnails/2.jpeg","thumbnails/3.jpeg"],
+  },
+  cup1: {
+    projectName: "cup1",
+    projectDescription: "Super cool",
+    projectImages: ["1.jpeg","2.jpeg"],
+    projectThumbnails: ["thumbnails/1.jpeg","thumbnails/2.jpeg"],
+  },
+  cup2: {
+    projectName: "cup2",
+    projectDescription: "Wicked",
+    projectImages: ["1.webp","2.webp","3.webp","4.webp"],
+    projectThumbnails: ["thumbnails/1.webp","thumbnails/2.webp","thumbnails/3.webp","thumbnails/4.webp"],
+  },
+  cup3: {
+    projectName: "cup3",
+    projectDescription: ";lkjasdf;lkjasfj;",
+    projectImages: ["1.webp"],
+    projectThumbnails: ["thumbnails/1.webp"],
+  },
+  cup5: {
+    projectName: "cup5",
+    projectDescription: "What the heck",
+    projectImages: ["1.avif"],
+    projectThumbnails: ["thumbnails/1.avif"],
+  },
+};
